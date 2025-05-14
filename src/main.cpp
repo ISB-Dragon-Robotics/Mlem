4e564c7097cab0290f298d32d59edeb6d4300345
Patrick Young <101240625+hellohelloju1@users.noreply.github.com>
2025-05-14 12:23:52 +0800

    YesTest

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,7 @@
  * "I was pressed!" and nothing.
  */
 void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
+
 }
 
 /**
